ec94cfaa27969ac6d2acd121c55adf659fa9ce72
Slava_desktop <rsramzesslava@gmail.com>
2023-11-12 22:23:59 +0300

    Done TMatrix.h

---
+++ Source.cpp
@@ -0,0 +1,24 @@
+#include "TMatrix.h"
+
+int main()
+{
+	/*
+	* TMatrix operator +(const TMatrix& matrix);
+	TMatrix operator -(const TMatrix& matrix);
+	TMatrix operator *(TMatrix& matrix);
+	TMatrix& operator +=(const TMatrix& matrix);
+	TMatrix& operator -=(const TMatrix& matrix);
+	TVector<MyType>& operator [](int index);
+	TVector<MyType>& operator [](int index) const;
+
+	bool operator ==(const TMatrix& matrix) const;
+	bool operator !=(const TMatrix& matrix) const;
+
+	double Determinant();
+
+	friend std::ostream& operator <<(std::ostream& os, const TMatrix& matrix);
+	friend std::istream& operator >>(std::istream& os, TMatrix& matrix);
+	*/
+
+
+}
--- TMatrix.h
+++ TMatrix.h
@@ -1,5 +1,32 @@
 #pragma once
+#include "TVector.h"
+#include <cmath>
+#define MyType int
+
 class TMatrix
 {
+	TVector<TVector <MyType>> arr;
+	unsigned int size_lines, size_columns;
+
+public:
+
+	explicit TMatrix(int n);
+	TMatrix(int n, int m);
+	
+	TMatrix operator +(const TMatrix& matrix);
+	TMatrix operator -(const TMatrix& matrix);
+	TMatrix operator *(TMatrix& matrix);
+	TMatrix& operator +=(const TMatrix& matrix);
+	TMatrix& operator -=(const TMatrix& matrix);
+	TVector<MyType>& operator [](int index);
+	TVector<MyType>& operator [](int index) const;
+
+	bool operator ==(const TMatrix& matrix) const;
+	bool operator !=(const TMatrix& matrix) const;
+
+	double Determinant();
+
+	friend std::ostream& operator <<(std::ostream& os, const TMatrix& matrix);
+	friend std::istream& operator >>(std::istream& os, TMatrix& matrix);
 };
 
